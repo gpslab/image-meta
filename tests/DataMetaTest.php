951ec4fb6d81c67b3695b518d673831e4b06d8e5
Peter Gribanov <wcode404@gmail.com>
2016-09-16 18:51:31 +0300

    test DataMeta object

---
+++ tests/DataMetaTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Peter Gribanov <info@peter-gribanov.ru>
+ */
+
+namespace GpsLab\Component\ImageMeta\Tests;
+
+use GpsLab\Component\ImageMeta\DataMeta;
+
+class DataMetaTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetters()
+    {
+        $width = 100;
+        $height = 200;
+        $mime = 'image/jpeg';
+
+        $data = new DataMeta($width, $height, $mime);
+
+        $this->assertEquals($width, $data->getWidth());
+        $this->assertEquals($height, $data->getHeight());
+        $this->assertEquals($mime, $data->getMime());
+
+        $this->assertEquals([
+            'width' => $width,
+            'height' => $height,
+            'mime' => $mime,
+        ], $data->toArray());
+    }
+}
