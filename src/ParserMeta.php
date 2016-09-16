66817c4fb730af5fa0e4975c1cf038eeef4011e1
Peter Gribanov <wcode404@gmail.com>
2016-09-16 18:01:56 +0300

    not require symfony/filesystem

--- src/ParserMeta.php
+++ src/ParserMeta.php
@@ -5,23 +5,8 @@
 
 namespace GpsLab\Component\ImageMeta;
 
-use Symfony\Component\Filesystem\Filesystem;
-
 class ParserMeta
 {
-    /**
-     * @var Filesystem
-     */
-    protected $fs;
-
-    /**
-     * @param Filesystem $fs
-     */
-    public function __construct(Filesystem $fs)
-    {
-        $this->fs = $fs;
-    }
-
     /**
      * @param string $path
      *
@@ -29,7 +14,7 @@ class ParserMeta
      */
     public function getMeta($path)
     {
-        if ($this->fs->exists($path) && ($info = @getimagesize($path))) {
+        if (file_exists($path) && ($info = @getimagesize($path))) {
             return new DataMeta($info[0], $info[1], $info['mime']);
         } else {
             return null;
